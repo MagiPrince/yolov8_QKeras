7f1f9f6bc93a1d550f6149b092c85e04acd40080
David Nogueiras <david.nogueiras@outlook.com>
2023-05-23 13:24:21 +0200

    changing forward functions to call, fixing some minor issues, and continuing developping modules of yolov8

--- common.py
+++ common.py
@@ -2,14 +2,14 @@ import tensorflow as tf
 from keras.layers import *
 
 class Conv(tf.keras.layers.Layer):
-    def __init__(self, c_out, kernel=1, stride=1, padding="same", groups=1):
+    def __init__(self, c_out, kernel_size=1, stride=1, padding="same", dilation_rate=1, groups=1):
         super(Conv, self).__init__()
 
-        self.conv = Conv2D(filters=c_out, kernel_size=kernel, strides=stride, padding=padding, groups=groups, use_bias=False)
-        self.bn = BatchNormalization(momentum=0.95)
+        self.conv = Conv2D(filters=c_out, kernel_size=kernel_size, strides=stride, padding=padding, dilation_rate=1, groups=groups, use_bias=False)
+        self.bn = BatchNormalization(momentum=0.99)
         self.relu = ReLU()
 
-    def forward(self, inputs):
+    def call(self, inputs):
         x = self.conv(inputs)
         x = self.bn(x)
         return self.relu(x)
@@ -22,10 +22,10 @@ class Bottleneck(tf.keras.layers.Layer):
         self.shortcut = shortcut
         self.c_out = c_out
         self.c = int(c_out * e)
-        self.conv1 = Conv(c_out=self.c, kernel=3, stride=1)
-        self.conv2 = Conv(c_out=c_out, kernel=3, stride=1, groups=groups)
+        self.conv1 = Conv(c_out=self.c, kernel_size=3, stride=1)
+        self.conv2 = Conv(c_out=c_out, kernel_size=3, stride=1, groups=groups)
 
-    def forward(self, inputs):
+    def call(self, inputs):
         in_shape = tf.shape(inputs)
         if self.shortcut and in_shape[-1] == self.c_out:
             return inputs + self.conv2(self.conv1(inputs))
@@ -42,24 +42,40 @@ class C2f(tf.keras.layers.Layer):
         self.conv2 = Conv(c_out=c_out)
         self.bottlenecks = [Bottleneck(c_out=self.c, shortcut=shortcut, groups=groups, e=1) for _ in range(n)]
 
-    def forward(self, inputs):
+    def call(self, inputs):
         x = self.conv1(inputs)
         y = tf.split(x, num_or_size_splits=2, axis=-1)
         y.extend(bottleneck(y[-1]) for bottleneck in self.bottlenecks)
-        return self.conv2(tf.keras.layers.Concatenate(axis=-1)(y))
+        return self.conv2(Concatenate(axis=-1)(y))
 
 
 class SPPF():
-    def __init__(self, c_out):
+    def __init__(self, c_in, c_out, kernel_size=5):
         super(SPPF, self).__init__()
 
-    def forward(self, inputs):
-        return None
+        c_ = c_in // 2
+        self.conv1 = Conv(c_out=c_)
+        self.conv2 = Conv(c_out=c_out)
+        self.maxpool = MaxPooling2D(pool_size=(kernel_size, kernel_size), strides=1, padding="same")
+
+    def call(self, inputs):
+        x = self.conv1(inputs)
+        y1 = self.maxpool(x)
+        y2 = self.maxpool(y1)
+        y3 = self.maxpool(y2)
+        concat = Concatenate(axis=-1)([x, y1, y2, y3])
+        return self.conv2(concat)
     
 
 class Detect():
     def __init__(self, c_out, nc=1):
         super(Detect, self).__init__()
 
-    def forward(self, inputs):
+        self.nc = nc  # number of classes
+        self.reg_max = 16  # DFL channels (ch[0] // 16 to scale 4/8/12/16/20 for n/s/m/l/x)
+
+        self.conv1 = Conv(c_out=c_out, kernel_size=3, stride=1)
+        self.conv2 = Conv(c_out=c_out, kernel_size=3, stride=1)
+
+    def call(self, inputs):
         return None
--- test_modules.py
+++ test_modules.py
@@ -2,14 +2,14 @@ import tensorflow as tf
 from keras.layers import *
 from common import *
 
-t = tf.ones([1, 3, 8, 5])
+t = tf.ones([1, 8, 8, 3])
 print(t)
 print(t.shape)
 
-t_temp = Conv(16, 3, 2).forward(t)
-t_temp = Conv(32, 3, 2).forward(t_temp)
+t_temp = Conv(16, 3, 2)(t)
+t_temp = Conv(32, 3, 2)(t_temp)
 
 print(t_temp)
 print(t_temp.shape)
-t_temp = C2f(32, True, 1).forward(t_temp)
+t_temp = C2f(32, True, 1)(t_temp)
 print(t_temp)
